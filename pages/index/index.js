d99df9b67caadfe63123612dc32d95487e58ba35
1234qwer <chenzt>
2021-03-03 17:33:45 +0800

    修改首页商品显示方式

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,5 +1,4 @@
 let App = getApp();
-
 Page({
   data: {
     // banner轮播组件属性
@@ -9,74 +8,15 @@ Page({
     duration: 800, // 滑动动画时长
     imgHeights: {}, // 图片的高度
     imgCurrent: {}, // 当前banne所在滑块指针
-
-
-   // 首页导航栏内容
-    /*
-    参数说明：
-      id: 跳转商品分类id
-      name: 名称，
-      imgUrl: 导航地址
-    */ 
-   navList:[
-    // {
-    //   id:1,                                 
-    //   name:'数码通讯',                      
-    //   imgSrc:'../../images/navImg/test.png',
-    // },
-    // {
-    //   id:1,                                 
-    //   name:'数码通讯',                      
-    //   imgSrc:'../../images/navImg/test.png',
-    // },
-    // {
-    //   id:1,                                 
-    //   name:'数码通讯',                      
-    //   imgSrc:'../../images/navImg/test.png',
-    // },
-    // {
-    //   id:1,                                 
-    //   name:'数码通讯',                      
-    //   imgSrc:'../../images/navImg/test.png',
-    // },
-    // {
-    //   id:1,                                 
-    //   name:'数码通讯',                      
-    //   imgSrc:'../../images/navImg/test.png',
-    // },
-    // {
-    //   id:1,                                 
-    //   name:'数码通讯',                      
-    //   imgSrc:'../../images/navImg/test.png',
-    // },
-    // {
-    //   id:1,                                 
-    //   name:'数码通讯',                      
-    //   imgSrc:'../../images/navImg/test.png',
-    // },
-    // {
-    //   id:1,                                 
-    //   name:'数码通讯',                      
-    //   imgSrc:'../../images/navImg/test.png',
-    // },
-    // {
-    //   id:1,                                 
-    //   name:'数码通讯',                      
-    //   imgSrc:'../../images/navImg/test.png',
-    // },
-    // {
-    //   id:1,                                 
-    //   name:'数码通讯',                      
-    //   imgSrc:'../../images/navImg/test.png',
-    // },
-  ], 
-
     // 页面元素
     ads: {},
     newest: {},
     best: {},
-
     scrollTop: 0,
+    option: {},
+    pageNumber: 1,
+    scrollHeight: null,
+    curNav: 121,
   },
 
 /**导航菜单的跳转路径**/
@@ -92,14 +32,22 @@ goNav(event){
 
 },
 
-
-  onLoad: function() {
+  onLoad: function(option) {
+    var that = this;
     // 设置页面标题
     App.setTitle();
     // 设置navbar标题、颜色
     App.setNavigationBar();
     // 获取首页数据
     this.getIndexData();
+
+    // 设置商品列表高度
+    that.setListHeight();
+     // 记录option
+     that.setData({ option }, function () {
+      // 获取商品列表
+      that.getProductList(true);
+    });
   },
 
   /**
@@ -111,6 +59,65 @@ goNav(event){
       _this.setData(result.data);
     });
   },
+
+
+  
+  /**
+   * 获取商品列表
+   */
+  getProductList: function (is_super, pageNumber) {
+    let _this = this;
+    App._get('/api/product/list', {
+      pageNumber: pageNumber || 1,
+      orderType: 'PRICE_DESC',
+      productCategoryId: 0
+    }, function (result) {
+      let resultList = result.data
+        , dataList = _this.data.list;
+      if (is_super === true || typeof dataList === 'undefined') {
+        _this.setData({ list: resultList, noList: false });
+      } else {
+        _this.setData({ 'list.productItems': dataList.productItems.concat(resultList.productItems) });
+      }
+      
+    });
+  },
+
+  selectNav: function (t) {
+    let curNav = t.target.dataset.id;
+    this.setData({
+      curNav,
+      scrollTop: 0
+    });
+    this.getProductList(true);
+  },
+
+    /**
+   * 下拉到底加载数据
+   */
+  bindDownLoad: function () {
+    // 已经是最后一页
+    if (this.data.pageNumber >= this.data.lastPage) {
+      this.setData({ no_more: true });
+      return false;
+    }
+    this.getProductList(false, ++this.data.pageNumber);
+  },
+
+  /**
+   * 设置商品列表高度
+   */
+  setListHeight: function () {
+    let _this = this;
+    wx.getSystemInfo({
+      success: function (res) {
+        _this.setData({
+          scrollHeight: res.windowHeight - 90,
+        });
+      }
+    });
+  },
+
 
   /**
    * 计算图片高度
@@ -164,8 +171,6 @@ goNav(event){
     });
   },
 
-
-  
    //分享到朋友圈
    onShareTimeline: function (res) {
     let _this = this;
