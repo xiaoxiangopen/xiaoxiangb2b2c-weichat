d78f70d5117359cec6c024a49cd65038e5d93830
1234qwer <chenzt>
2021-03-07 18:46:51 +0800

    增加产品URL说明

--- siteinfo.js
+++ siteinfo.js
@@ -3,6 +3,7 @@
  */
 module.exports = {
   name: "小象电商",
+  //开源产品直接连接这个连接运行即可
   siteroot: "https://newmall.cgzfs.com/", // 必填: api地址，结尾要带/
     //  siteroot: "http://localhost:8080/", // 必填: api地址，结尾要带/
     //  siteroot: "https://test003.cgzfs.com/", // 必填: api地址，结尾要带/
